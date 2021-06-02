c7de5d53702aae4a3ab29b9b7d566ef765758ef5
emilyvictoriawyatt <83621325+emilyvictoriawyatt@users.noreply.github.com>
2021-06-02 19:44:16 +1000

    Update sketch_210601a.js

--- SkeletonsSampler/sketch_210601a.js
+++ SkeletonsSampler/sketch_210601a.js
@@ -1,13 +1,8 @@
 var analyzer;
-var button ;
 var skr;
-var onScreenText = "We're all Skeletons out here";
 var songsk;
 var songTime;
-var sliderVolume;
-var sliderRate;
 var togglePlaying;
-
 const letters = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*1234567890`;
 var listLength;
 
@@ -32,22 +27,42 @@ function setup() {
 
 function draw() {
   background(0);
+  
+
+  
+  if (songsk.isPlaying() == false){
+     textSize(20);
+    textFont(skb);
+    noStroke();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("Warning: Sound and strobe lights", width/2, height/2);
+  }
+      else {
+      }
+
+  
   var volume = analyzer.getLevel();
   songTime = songsk.currentTime();
 
   for (let x = 0; x <= width; x += 250) {
   for (let y = 0; y <= height; y += 100) {
+
     
   if ((songTime > 0) && (songTime < 4))  {
+    push();
     noStroke();
+    strokeWeight(3);
     textFont (skb);
     textSize(volume*200);
     fill(random(255), random(255), 255);
     frameRate(5);
-    text('-----------', random (x), random (y)); 
+    text('-----------', random (width), random (height)); 
+    pop();
 }   
     
   if ((songTime > 4) && (songTime < 6))  {
+    push();
     background (random(255));
     frameRate(10);
     noStroke();
@@ -56,9 +71,11 @@ function draw() {
     fill(random(255));
     textAlign (CENTER, CENTER);
     text("i think i can hear something...", width/2, height/2); 
+    pop();
 }   
     
   if ((songTime > 6) && (songTime < 9))  {
+    push();
     background (random(255));
     frameRate(10);
     noStroke();
@@ -67,9 +84,11 @@ function draw() {
     fill(random(255));
     textAlign (CENTER, CENTER);
     text("in there, in the closet", width/2.7, height/2.7);   
+    pop();
 }   
 
   if ((songTime > 9) && (songTime < 12.5))  {
+    push();
     background (random(255));
     frameRate(10);
     noStroke();
@@ -78,9 +97,11 @@ function draw() {
     fill(random(255));
     textAlign (CENTER, CENTER);
     text("help me", width/1.8, height/2.6);   
+    pop();
 }   
 
   if ((songTime > 12.5) && (songTime < 16))  {
+    push();
     background (random(255), random(255), 255);
     songsk.setVolume(2);
     frameRate(10);
@@ -88,31 +109,35 @@ function draw() {
     noFill();
     stroke(255);
     ellipse(random (x), random (y), random(width));
+    pop();
 }   
 
   if ((songTime > 16) && (songTime < 19))  {
+    push();
     rand = int(random (0,listLength-1));
-    //background (random(255), random(255), 255);
     songsk.setVolume(2);
-    stroke(random(255), random(255), 255);
-    textFont (skr);
+    textFont (skb);
     textSize(volume*100);
     fill (random(255), random(255), 255);
-    strokeWeight(3);
+    noStroke();
     frameRate(5);
     text(letters[rand], random(width), random(height));   
+    pop();
 }   
 
   if ((songTime > 19) && (songTime < 22))  {
+    push();
     noStroke();
     textFont (skb);
     textSize(random (5, 30));
     fill(random(255), random(255), 255);
     frameRate(5);
     text('?', x, y); 
+    pop();
 }   
 
   if ((songTime > 22) && (songTime < 24))  {
+    push();
     background(0);
     noStroke();
     textFont (skb);
@@ -120,31 +145,39 @@ function draw() {
     fill(random(255), random(255), 255);
     frameRate(5);
     text('Skeletons in your closet', width/2, height/2); 
+    pop();
 
 }
 
   if ((songTime > 24) && (songTime < 25.5)) {
-     noFill();
-     strokeWeight(2);
-     stroke(random(200), random(200), 255);
-     ellipse(x, y, random(width));
+    push();
+    fill(random(200), random(200), 255);
+    textFont (skb);
+    textSize (30, 100);
+    text('*', random (width), random (height));
+    pop();
 }
 
   if ((songTime > 25.5) && (songTime < 28))  {
-    noFill();
-    strokeWeight(2);
-    stroke(random(200), random(200), 255);
-    ellipse(x, y, random(width));
+    push();
+    fill(random(200), random(200), 255);
+    textFont (skb);
+    textSize (30, 100);
+    text('*', random (width), random (height));
+    pop();
+    
+    push();
     noStroke();
     textFont (skb);
     textSize (volume*70);
     fill(255);
     frameRate(5);
-    text("Itchin' to come outside", width/2, height/2); 
+    text("Itchin' to come outside", width/2, height/2);
+    pop();
 }
 
-
   if ((songTime > 28) && (songTime < 32))  {
+    push();
     background (random(255), random(255), 255);
     songsk.setVolume(2);
     frameRate(5);
@@ -152,9 +185,11 @@ function draw() {
     noFill();
     stroke(255);
     rect(random (x), random (y), random(width), random(height));
+    pop();
 }  
 
   if ((songTime > 32) && (songTime < 35))  {
+    push();
     songsk.setVolume(2);
     frameRate(5);
     strokeWeight(5);
@@ -169,20 +204,22 @@ function draw() {
     rotate(volume);
     textAlign(CENTER, CENTER);
     text("Messin' with your conscience", width/2, height/2); 
+    pop();
 }  
 
   if ((songTime > 35) && (songTime < 41)) {
+    push();
     noStroke();
     textFont (skb);
     textSize(random (5, 30));
     fill(random(255), random(255), 255);
     frameRate(5);
     text("In a way your face can't hide", x, y); 
+    pop();
     } 
    }
-  }
  }
-
+}
 
 function togglePlaying(){
 }
@@ -190,10 +227,8 @@ function togglePlaying(){
 function mousePressed(){
     if (songsk.isPlaying()) {
     songsk.pause();
-    //songsk.noloop();
   } else {
     songsk.play();
-    //songsk.loop();
   }
 }
 
@@ -202,4 +237,6 @@ function windowResized(){
   resizeCanvas (windowWidth, windowHeight);
   
 }
+
 // this sampler was inspired by https://editor.p5js.org/anniezhengg/sketches/xaaDdu3NW
+// Made with p5.js
